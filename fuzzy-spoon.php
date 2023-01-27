af50f8600935321e4fe9159a054da446ae47acf3
Abhijitsarker <abhijitsarker03@gmail.com>
2023-01-27 10:37:18 +0600

    first commit

---
+++ fuzzy-spoon.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Fuzzy Spoon
+ * Plugin URI: https://abhijitsarker.github.io/my-portfolio/
+ * Description: Custom plugin for wordpress development
+ * Version: 1.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: Abhijit Sarker
+ * Author URI: https://abhijitsarker.github.io/my-portfolio/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI: https://example.com/my-plugin/
+ * Text Domain: fuzzy-spoon
+ * Domain Path: /languages
+ */
+
+
+if (!defined('ABSPATH')) {
+    die;
+}
