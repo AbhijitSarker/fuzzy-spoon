f7e39c9722ab7fd622c62ef2c54318d667241976
Abhijitsarker <abhijitsarker03@gmail.com>
2023-03-04 22:16:28 +0600

    main structure part

--- fuzzy-spoon.php
+++ fuzzy-spoon.php
@@ -20,3 +20,74 @@
 if (!defined('ABSPATH')) {
     die;
 }
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+//main plugin class 
+final class Fuzzy_Spoon
+{
+    //plugin version
+    const VERSION  = '1.0';
+
+    //class constructor
+    private function __construct()
+    {
+        $this->define_constants();
+
+        register_activation_hook(__FILE__, [$this, 'activate']);
+
+        add_action($this, [$this, 'init_plugin']);
+    }
+
+    //initialize a singleton instance
+    //return fuzzy-spoon
+    public static function init()
+    {
+        static $instance = false;
+
+        if (!$instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+    //defining constants
+    public function define_constants()
+    {
+        define('FUZZY_SPOON_VERSION', self::VERSION);
+        define('FUZZY_SPOON_FILE', __FILE__);
+        define('FUZZY_SPOON_PATH', __DIR__);
+        define('FUZZY_SPOON_URL', plugins_url('', FUZZY_SPOON_FILE));
+        define('FUZZY_SPOON_ASSETS', FUZZY_SPOON_URL . '/assets');
+    }
+
+    //
+    public function init_plugin()
+    {
+    }
+
+
+    //D o stuff on activation
+    public function activate()
+    {
+        $installed = get_option('fuzzy_spoon_installed');
+
+        if (!$installed) {
+            update_option('fuzzy_spoon_installed', time());
+        }
+
+        update_option('fuzzy_spoon_version', FUZZY_SPOON_VERSION);
+    }
+}
+
+//initialize the main plugin
+//return \fuzzy-spoon
+function fuzzy_spoon()
+{
+    return Fuzzy_Spoon::init();
+}
+
+
+//kick off the plugin
+fuzzy_spoon();
---
+++ includes/Admin/Menu.php
@@ -0,0 +1,12 @@
+<<?php
+
+    namespace Fuzzy\Spoon\Admin\Menu;
+
+    //the menu handler class
+
+    // class Menu
+    // {
+    //     function __construct()
+    //     {
+    //     }
+    // }
